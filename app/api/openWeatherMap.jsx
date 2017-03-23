013d706ed501a4199a1dc9ab9f1764ac7ea1249d
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-03-22 20:46:05 -0600

    error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -8,7 +8,6 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function(res){
-      debugger;
       if(res.data.cod && res.data.message){
         throw new Error(res.data.message);
       }
@@ -16,7 +15,7 @@ module.exports = {
         return res.data.main.temp;
       }
     }, function(res){
-      throw new Error(res.data.message);
+      throw new Error("Location not found");
     });
   }
 }
